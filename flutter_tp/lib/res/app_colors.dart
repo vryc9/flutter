79c0390701eb98a2e65151b52aeb4d164d9bdf55
Prop <51509630+Propleek@users.noreply.github.com>
2024-11-29 16:32:35 +0100

    icons and colors

---
+++ flutter_tp/lib/res/app_colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color screenBackground = Color(0xFF15232E);
+  static const Color orange = Color(0xFFFF8100);
+  static const Color cardBackground = Color(0xFF1E3243);
+  static const Color cardElementBackground = Color(0xFF284C6A);
+  static const Color seeMoreBackground = Color(0xFF0F1921);
+  static const Color bottomBarBackground = Color(0xFF0F1E2B);
+  static const Color bottomBarBackgroundSelected = Color(0xFF12273C);
+  static const Color bottomBarTextSelected = Color(0xFF12273C);
+  static const Color bottomBarTextUnselected = Color(0xFF778BA8);
+}
---
+++ flutter_tp/lib/res/app_svg.dart
@@ -0,0 +1,15 @@
+class AppVectorialImages {
+  AppVectorialImages._();
+
+  static const String astronaut = 'res/svg/astronaut.svg';
+  static const String icBooksBicolor = 'res/svg/ic_books_bicolor.svg';
+  static const String icCalendarBicolor = 'res/svg/ic_calendar_bicolor.svg';
+  static const String icMovieBicolor = 'res/svg/ic_movie_bicolor.svg';
+  static const String icPublisherBicolor = 'res/svg/ic_publisher_bicolor.svg';
+  static const String icTvBicolor = 'res/svg/ic_tv_bicolor.svg';
+  static const String navbarComics = 'res/svg/navbar_comics.svg';
+  static const String navbarHome = 'res/svg/navbar_home.svg';
+  static const String navbarMovies = 'res/svg/navbar_movies.svg';
+  static const String navbarSearch = 'res/svg/navbar_search.svg';
+  static const String navbarSeries = 'res/svg/navbar_series.svg';
+}
