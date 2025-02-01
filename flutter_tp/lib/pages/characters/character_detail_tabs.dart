03a355ef4d11e7be9f0a89ea718f26b5d8bc7250
leane <cardonacouffin@et.esiea.fr>
2025-02-01 19:41:54 +0100

    import inutile retiré + readme à jour

--- flutter_tp/lib/pages/characters/character_detail_tabs.dart
+++ flutter_tp/lib/pages/characters/character_detail_tabs.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_tp/res/app_colors.dart';
 import 'package:flutter_tp/res/app_svg.dart';
