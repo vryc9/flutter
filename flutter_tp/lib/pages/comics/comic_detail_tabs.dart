486f51e406568f4bb095c246563ac38efab6554f
Prop <51509630+Propleek@users.noreply.github.com>
2025-02-01 20:23:15 +0100

    detail header

--- flutter_tp/lib/pages/comics/comic_detail_tabs.dart
+++ flutter_tp/lib/pages/comics/comic_detail_tabs.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_tp/res/app_colors.dart';
+import 'package:flutter_tp/res/app_svg.dart';
+import 'package:flutter_tp/utils/date_format.dart';
 import 'package:flutter_tp/utils/text_formatter_utils.dart';
+import 'package:flutter_tp/widgets/header_detail.dart';
 
 import '../../model/comic_api.dart';
 import '../../widgets/histoire_detail.dart';
@@ -12,33 +17,110 @@ class ComicDetailTabs extends StatelessWidget {
   const ComicDetailTabs({super.key, required this.comic});
 
   @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(getDefaultTextForEmptyValue(comic.name, defaultValue: "Nom indisponible")),
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: "Histoire"),
-              Tab(text: "Auteurs"),
-              Tab(text: "Personnages"),
-            ],
+Widget build(BuildContext context) {
+  return DefaultTabController(
+    length: 3,
+    child: Column(
+      children: [
+        HeaderWidget(
+          title: getDefaultTextForEmptyValue(comic.name, defaultValue: "Nom indisponible"),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.all(12.0), 
+          child: Container(
+          
+            color: AppColors.screenBackground, // Background color (customize as needed)
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.25, // 1/4 of screen width
+                  height: 250, // Fixed height (adjustable)
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8), // Rounded corners
+                    image: DecorationImage(
+                      image: NetworkImage(comic.image!.original_url!), 
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 12), 
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          SvgPicture.asset(
+                            AppVectorialImages.icBooksBicolor,
+                            height: 15.0,
+                            colorFilter: const ColorFilter.mode(
+                                AppColors.bottomBarTextUnselected, BlendMode.srcIn),
+                          ),
+                          const SizedBox(width: 8.0),
+                          Text(
+                            'N°${comic.issue_number ?? 'Non défini'}',
+                            style: const TextStyle(
+                              color: AppColors.bottomBarTextUnselected,
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 4), // Space between texts
+                      Row(
+                        children: [
+                          SvgPicture.asset(
+                            AppVectorialImages.icCalendarBicolor,
+                            height: 15.0,
+                            colorFilter: const ColorFilter.mode(
+                                AppColors.bottomBarTextUnselected, BlendMode.srcIn),
+                          ),
+                          const SizedBox(width: 8.0),
+                          Text(
+                            formatDateMonthYear(comic.cover_date),
+                            style: const TextStyle(
+                              color: AppColors.bottomBarTextUnselected,
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),  
+        ),
+        
+        Expanded(
+          child: Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              bottom: const TabBar(
+                tabs: [
+                  Tab(text: "Histoire"),
+                  Tab(text: "Auteurs"),
+                  Tab(text: "Personnages"),
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: [
+                _buildStoryTab(),
+                _buildAuthorsTab(),
+                _buildCharactersTab(),
+              ],
+            ),
           ),
         ),
-        body: TabBarView(
-          children: [
-            // Onglet Histoire
-            _buildStoryTab(),
-            // Onglet Auteurs
-            _buildAuthorsTab(),
-            // Onglet Personnages
-            _buildCharactersTab(),
-          ],
-        ),
-      ),
-    );
-  }
+      ],
+    ),
+  );
+}
+
 
   Widget _buildStoryTab() {
     return Padding(
